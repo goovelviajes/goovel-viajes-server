c7a9555008621254135d470d9cff664918aa6277
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-09-02 11:44:06 -0300

    Created message entity and it's relations

---
+++ src/message/entities/message.entity.ts
@@ -0,0 +1,23 @@
+import { User } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Message {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    content: string;
+
+    @Column({ name: 'is_read', default: false })
+    isRead: boolean;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @ManyToOne(() => User, (user) => user.sentMessages, { onDelete: 'CASCADE' })
+    sender: User;
+
+    @ManyToOne(() => User, (user) => user.receivedMessages, { onDelete: 'CASCADE' })
+    receiver: User;
+}
