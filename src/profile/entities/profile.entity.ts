4864ae98f54d61f503e4c7e224072ff0ba7b3b6b
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-09-01 21:31:29 -0300

    Created profile resource, and it's relations

---
+++ src/profile/entities/profile.entity.ts
@@ -0,0 +1,33 @@
+import { User } from "src/user/entities/user.entity";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Profile {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ unique: true, name: 'profile_name' })
+    profileName: string;
+
+    @Column({ nullable: true })
+    phone: string;
+
+    @Column({ nullable: true })
+    image: string;
+
+    @Column({ nullable: true })
+    address: string;
+
+    @Column({ nullable: true })
+    city: string;
+
+    @Column({ nullable: true })
+    country: string;
+
+    @Column({ nullable: true })
+    province: string;
+
+    @OneToOne(() => User, (user) => user.profile, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+}
