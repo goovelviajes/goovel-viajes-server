1e9f15919153f8c16f45c3cb40bbabc082bc96c0
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-09-02 18:27:18 -0300

    Created rating entity and it's relations

---
+++ src/rating/entities/rating.entity.ts
@@ -0,0 +1,27 @@
+import { Journey } from "src/journey/entities/journey.entity";
+import { User } from "src/user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Rating {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    rating: Number;
+
+    @Column()
+    comment: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @ManyToOne(() => Journey, (journey) => journey.ratings, { onDelete: 'CASCADE' })
+    journey: Journey;
+
+    @ManyToOne(() => User, (user) => user.givenRatings, { onDelete: 'CASCADE' })
+    raterUser: User;
+
+    @ManyToOne(() => User, (user) => user.receivedRatings, { onDelete: 'CASCADE' })
+    ratedUser: User;
+}
