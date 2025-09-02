f861f92d89b5a4bb5f322408d58dcb70284c9083
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-09-02 18:28:36 -0300

    Created relations among user entity and the rest of entities

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,16 +1,24 @@
 
 // import { AuthProvider } from 'src/auth/enums/auth-provider.enum';
+import { Notification } from 'src/notification/entities/notification.entity';
+import { Profile } from 'src/profile/entities/profile.entity';
+import { Report } from 'src/report/entities/report.entity';
 import {
   Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  OneToMany,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from 'typeorm';
 import { RolesEnum } from '../enums/roles.enum';
-import { Profile } from 'src/profile/entities/profile.entity';
+import { Message } from 'src/message/entities/message.entity';
+import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
+import { Journey } from 'src/journey/entities/journey.entity';
+import { Booking } from 'src/booking/entities/booking.entity';
+import { Rating } from 'src/rating/entities/rating.entity';
 
 @Entity()
 export class User {
@@ -44,8 +52,35 @@ export class User {
   @Column({ default: RolesEnum.MEMBER })
   role: RolesEnum;
 
-  @OneToOne(() => Profile, (profile) => profile.user)
+  @OneToOne(() => Profile, (profile) => profile.user, { cascade: true })
   profile: Profile;
+
+  @OneToMany(() => Notification, (notification) => notification.user)
+  notifications: Notification[];
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
+
+  @OneToMany(() => Vehicle, (vehicle) => vehicle.user)
+  vehicles: Vehicle[];
+
+  @OneToMany(() => Message, (message) => message.sender)
+  sentMessages: Message[];
+
+  @OneToMany(() => Message, (message) => message.receiver)
+  receivedMessages: Message[];
+
+  @OneToMany(() => Journey, (journey) => journey.user)
+  journeys: Journey[]
+
+  @OneToMany(() => Booking, (booking) => booking.user)
+  bookings: Booking[];
+
+  @OneToMany(() => Rating, (rating) => rating.raterUser)
+  givenRatings: Rating[];
+
+  @OneToMany(() => Rating, (rating) => rating.ratedUser)
+  receivedRatings: Rating[];
 
   // ---> Habilitar para Google:
   // @Column({ type: 'enum', enum: AuthProvider, default: AuthProvider.LOCAL })
