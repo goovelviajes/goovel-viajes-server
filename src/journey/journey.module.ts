31717f8fdf16a0ea637fea4183b56e387a91475c
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-09-29 14:13:57 -0300

    GAM-93: type orm module features error

--- src/journey/journey.module.ts
+++ src/journey/journey.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JourneyService } from './journey.service';
 import { JourneyController } from './journey.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Journey } from './entities/journey.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Journey])],
   controllers: [JourneyController],
   providers: [JourneyService],
 })
-export class JourneyModule {}
+export class JourneyModule { }
