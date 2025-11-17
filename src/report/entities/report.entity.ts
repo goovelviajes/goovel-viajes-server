f5fed18921af7898e089b6dc20d4243caf71151b
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-11-17 13:44:45 -0300

    fix: add tags of Swagger at all contollers

--- src/report/entities/report.entity.ts
+++ src/report/entities/report.entity.ts
@@ -1,4 +1,4 @@
-import { User } from "src/user/entities/user.entity";
+import { User } from "../../user/entities/user.entity";
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
