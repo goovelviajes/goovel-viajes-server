093ed72db24a53b77f2aae2ef7348fc2bdacbb88
Tomás Cárdenas <tomicardenas96@gmail.com>
2026-01-09 14:19:03 -0300

    feat: create endpoint for grant professional verification badge to users as admin

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,11 +7,14 @@ import {
   UseGuards
 } from '@nestjs/common';
 import { ApiBadRequestResponse, ApiBearerAuth, ApiInternalServerErrorResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
-import { TokenGuard } from 'src/auth/guard/token.guard';
-import { ActiveUser } from 'src/common/decorator/active-user.decorator';
-import { ActiveUserInterface } from 'src/common/interface/active-user.interface';
+import { TokenGuard } from '../auth/guard/token.guard';
+import { ActiveUser } from '../common/decorator/active-user.decorator';
+import { ActiveUserInterface } from '../common/interface/active-user.interface';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
+import { GrantVerificationDto } from './dto/grant-verification.dto';
+import { RoleGuard } from '../auth/guard/role.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('user')
 export class UserController {
@@ -40,5 +43,18 @@ export class UserController {
   @HttpCode(204)
   softDeleteUser(@ActiveUser() { id }: ActiveUserInterface) {
     return this.userService.softDeleteUser(id);
+  }
+
+  @UseGuards(TokenGuard, RoleGuard)
+  @Roles('admin')
+  @ApiOperation({ summary: 'Dar verificación de profesional a un usuario' })
+  @ApiNoContentResponse()
+  @ApiNotFoundResponse({ description: 'User not found' })
+  @ApiInternalServerErrorResponse({ description: 'Error granting verification' })
+  @ApiBearerAuth('access-token')
+  @Patch('grant-verification')
+  @HttpCode(204)
+  grantVerification(@Body() { email }: GrantVerificationDto) {
+    return this.userService.grantVerification(email);
   }
 }
