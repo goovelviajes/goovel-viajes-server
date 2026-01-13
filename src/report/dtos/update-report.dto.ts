283a8837fbfa5baed245a6919e3b07533b5f6e11
Tomás Cárdenas <tomicardenas96@gmail.com>
2026-01-13 17:30:38 -0300

    feat: dto missing swagger decorators

--- src/report/dtos/update-report.dto.ts
+++ src/report/dtos/update-report.dto.ts
@@ -1,15 +1,29 @@
 import { IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator";
 import { ReportStatus } from "../enums/report-status.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateReportDto {
+    @ApiProperty({
+        enum: ReportStatus,
+        example: ReportStatus.PENDING,
+        description: 'Estado del reporte'
+    })
     @IsEnum(ReportStatus)
     status: ReportStatus;
 
+    @ApiProperty({
+        example: 'No cumplio con los terminos y condiciones',
+        description: 'Explicacion del admin sobre su decision '
+    })
     @IsString()
     @IsNotEmpty({ message: 'Admin note is required' })
     @MinLength(10, { message: 'Admin note must be at least 10 characters long' })
     adminNotes: string;
 
+    @ApiProperty({
+        example: true,
+        description: 'Si se activa, el usuario sera baneado inmediatamente'
+    })
     @IsOptional()
     @IsBoolean()
     banImmediately?: boolean;
